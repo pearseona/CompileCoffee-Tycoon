5749e547b697cb3c33f56f9ee6bb063bf71d6769
pearseona <baeseona2930@gmail.com>
2026-05-28 00:51:07 +0900

    refactor: render 디렉터리 분리 및 실시간 인게임 그래픽 모듈화

--- render.h
+++ render.h
@@ -1,1 +1,9 @@
-// SDL2 전체 렌더링
+#pragma once
+#include <SDL.h>
+#include <SDL_ttf.h>
+#include "./common.h"
+
+void render_frame(SDL_Renderer* ren, TTF_Font* fnt_lg, TTF_Font* fnt_md, TTF_Font* fnt_sm, Game* g);
+
+void draw_customer_queue(SDL_Renderer* ren, Game* g);
+void draw_barista_slots(SDL_Renderer* ren, Game* g);
